6cbceb57b756ec416477016b7bfeeee79d37a3bc
zmejchik <zmejchik@gmail.com>
2023-10-08 00:24:51 +0300

    Edit java script

--- js/slider.js
+++ js/slider.js
@@ -24,12 +24,37 @@ document.addEventListener('DOMContentLoaded', function() {
 
   document.querySelector('.btn-prev').addEventListener('click', function() {
     currentIndex = (currentIndex - 1 + 3) % 3; // 3 is the total number of slides
+    selectRadioButton(currentIndex);
     showSlide(currentIndex);
   });
 
   document.querySelector('.btn-next').addEventListener('click', function() {
     currentIndex = (currentIndex + 1) % 3; // 3 is the total number of slides
+    selectRadioButton(currentIndex);
     showSlide(currentIndex);
   });
+
+  function selectRadioButton(index) {
+    // Получаем все радио кнопки с указанным именем
+    var radioButtons = document.getElementsByName("slide");
+  
+    // Проверяем, что индекс находится в пределах допустимых значений
+    if (index >= 0 && index < radioButtons.length) {
+      // Сбрасываем выбор всех радио кнопок
+      for (var i = 0; i < radioButtons.length; i++) {
+        radioButtons[i].checked = false;
+      }
+  
+      // Устанавливаем выбор для радио кнопки с указанным индексом
+      radioButtons[index].checked = true;
+  
+      
+  }};
+  
+  
+  
+  
+  
+  
 });
 
