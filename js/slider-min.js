ab8428549389b560d20b18fa232b07c323254079
zmejchik <zmejchik@gmail.com>
2023-10-22 12:14:13 +0300

    Clear musor maket 2 as next

---
+++ js/slider-min.js
@@ -0,0 +1,54 @@
+var h1Element = document.querySelector('.intro-teatle h1');
+  var radioButtons = document.querySelectorAll('input[name="slide"]');
+  var currentIndex = 0;
+
+  function updateH1Text() {
+    var selectedRadioValue = document.querySelector('input[name="slide"]:checked').value;
+    h1Element.textContent = getH1Text(selectedRadioValue);
+  }
+
+  function getH1Text(radioValue) {
+    switch (radioValue) {
+      case "0":
+        return "МИСЛИ ГЛОБАЛЬНО";
+      case "1":
+        return "МИСЛИ ГЛОБАЛЬНО1";
+      case "2":
+        return "МИСЛИ ГЛОБАЛЬНО2";
+      default:
+        return "МИСЛИ ГЛОБАЛЬНО";
+    }
+  }
+
+  function updateH1TextSequentially(next) {
+    if (window.innerWidth <= 768) {
+      if (next) {
+        currentIndex = (currentIndex + 1) % radioButtons.length;
+      } else {
+        currentIndex = (currentIndex - 1 + radioButtons.length) % radioButtons.length;
+      }
+      radioButtons[currentIndex].checked = true;
+      updateH1Text();
+    }
+  }
+
+  // Update the text initially and when the window is resized or radio buttons change
+  updateH1Text();
+  window.addEventListener('resize', updateH1Text);
+
+  // Update the text when a radio button is changed
+  radioButtons.forEach(function (radioButton) {
+    radioButton.addEventListener('change', updateH1Text);
+  });
+
+  // Update the text when "btn-prev" is clicked
+  var btnPrev = document.querySelector('.btn-prev');
+  btnPrev.addEventListener('click', function () {
+    updateH1TextSequentially(false);
+  });
+
+  // Update the text when "btn-next" is clicked
+  var btnNext = document.querySelector('.btn-next');
+  btnNext.addEventListener('click', function () {
+    updateH1TextSequentially(true);
+  });
